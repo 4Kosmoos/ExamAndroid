c202cdfd081ab753a07ce0c256fc9d77c8858656
4Kosmoos <firmin.shu@gmail.com>
2025-03-11 22:34:21 +0100

    method update

--- app/src/main/java/edu/esiea/examandroid/repository/PlaceRepository.java
+++ app/src/main/java/edu/esiea/examandroid/repository/PlaceRepository.java
@@ -1,5 +1,8 @@
 package edu.esiea.examandroid.repository;
 
+import android.content.Context;
+
+import edu.esiea.examandroid.data.AppDatabase;
 import edu.esiea.examandroid.data.dao.PlaceDao;
 import edu.esiea.examandroid.data.dao.ChildPlaceEntity;
 import edu.esiea.examandroid.data.dto.PlaceWithDetails;
@@ -10,14 +13,17 @@ import edu.esiea.examandroid.data.entity.PlaceToExerciseEntity;
 import edu.esiea.examandroid.data.entity.PlaceToGoOutEntity;
 import edu.esiea.examandroid.data.entity.PlaceToRelaxEntity;
 import edu.esiea.examandroid.data.entity.PlaceToSleepEntity;
+import edu.esiea.examandroid.data.executor.ExecutorProvider;
 
 public class PlaceRepository {
 
     private final PlaceDao placeDao;
+    private final ExecutorProvider executor;
 
-    public PlaceRepository(PlaceDao placeDao) {
-        this.placeDao = placeDao;
-    }
+    public PlaceRepository(Context context) {
+        AppDatabase db = AppDatabase.getDatabase(context);
+        this.placeDao = db.placeDao();  // Initialisation correcte
+        this.executor = ExecutorProvider.getInstance();    }
 
     public void addPlace(PlaceEntity place, ChildPlaceEntity details) {
         int placeId = (int) placeDao.insertPlace(place);
@@ -53,6 +59,34 @@ public class PlaceRepository {
 
     public void removePlace(PlaceEntity place) {
         placeDao.deletePlace(place);
+    }
+
+    public void updatePlace(PlaceEntity updatedPlace, ChildPlaceEntity newDetails) {
+        placeDao.deletePlace(updatedPlace);
+        int newPlaceId = (int) placeDao.insertPlace(updatedPlace);
+        newDetails.setPlaceId(newPlaceId);
+        switch (updatedPlace.getType()) {
+            case PlaceToEat:
+                placeDao.insertPlaceToEat((PlaceToEatEntity) newDetails);
+                break;
+            case PlaceToSleep:
+                placeDao.insertPlaceToSleep((PlaceToSleepEntity) newDetails);
+                break;
+            case PlaceToGoOut:
+                placeDao.insertPlaceToGoOut((PlaceToGoOutEntity) newDetails);
+                break;
+            case PlaceToRelax:
+                placeDao.insertPlaceToRelax((PlaceToRelaxEntity) newDetails);
+                break;
+            case PlaceToExercise:
+                placeDao.insertPlaceToExercise((PlaceToExerciseEntity) newDetails);
+                break;
+            case CulturalPlace:
+                placeDao.insertCulturalPlace((CulturalPlaceEntity) newDetails);
+                break;
+            default:
+                break;
+        }
     }
 }
 
