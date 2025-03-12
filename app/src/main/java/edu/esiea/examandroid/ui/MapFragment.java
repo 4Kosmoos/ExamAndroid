94a8eef1c0699d18badcaafdab87320f7cc2d296
4Kosmoos <firmin.shu@gmail.com>
2025-03-12 16:26:52 +0100

    création map enfin fonctionnelle

--- app/src/main/java/edu/esiea/examandroid/ui/MapFragment.java
+++ app/src/main/java/edu/esiea/examandroid/ui/MapFragment.java
@@ -2,34 +2,63 @@ package edu.esiea.examandroid.ui;
 
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+
 import edu.esiea.examandroid.R;
+import edu.esiea.examandroid.viewmodel.PlaceViewModel;
 
 public class MapFragment extends Fragment {
 
     private Button buttonGoToList;
+    private MapView mapView;
+    private PlaceViewModel placeViewModel;
+
+    private ActivityResultLauncher<String> requestLocationPermissionLauncher;
+
+    private static final GeoPoint DEFAULT_CENTER = new GeoPoint(43.7196087, -1.0549204); // ESIEA
+    private static final double DEFAULT_ZOOM = 15.0;
 
     public MapFragment() {
-        // Required empty public constructor
     }
+
 @Nullable
 @Override
 public View onCreateView(@NonNull LayoutInflater inflater,
                          @Nullable ViewGroup container,
                          @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_map, container, false);
-    buttonGoToList = view.findViewById(R.id.button_go_to_list);
 
+    Configuration.getInstance().load(
+            requireContext(),
+            PreferenceManager.getDefaultSharedPreferences(requireContext())
+    );
+    View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+    mapView = view.findViewById(R.id.map);
+    mapView.setTileSource(TileSourceFactory.MAPNIK);
+    mapView.setMultiTouchControls(true);
+    mapView.getController().setZoom(DEFAULT_ZOOM);
+    mapView.getController().setCenter(DEFAULT_CENTER);
+
+    buttonGoToList = view.findViewById(R.id.button_go_to_list);
     buttonGoToList.setOnClickListener(v -> {
         NavController navController = NavHostFragment.findNavController(this);
         navController.navigate(R.id.listFragment);
@@ -37,4 +66,5 @@ public View onCreateView(@NonNull LayoutInflater inflater,
 
     return view;
 }
+
 }
---
+++ app/src/main/java/edu/esiea/examandroid/viewmodel/PlaceViewModel.java
@@ -0,0 +1,4 @@
+package edu.esiea.examandroid.viewmodel;
+
+public class PlaceViewModel {
+}
